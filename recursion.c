e0960c7a4b123271cbdfc7c95326897cff6876ff
chesta123 <76441931+chesta123@users.noreply.github.com>
2021-10-12 15:56:23 +0530

    my changes

--- recursion.c
+++ recursion.c
@@ -3,6 +3,7 @@ int main()
 {
     int x = 3;
     func1(x);
+    func2(x);
     return 0;
 }
 
@@ -16,4 +17,11 @@ void func1(int n)
     }
 }
 
-//hey there//
+void func2(int n)
+{
+    if (n>0)
+    {
+        func2(n-1);
+        printf("%d\n",n);
+    }
+}
